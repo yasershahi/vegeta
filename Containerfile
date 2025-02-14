c10fafed0921bfa1cdae0f6aa7f1de08acbb091b
Yaser <yaserim@duck.com>
2025-02-14 06:16:40 +0330

    Refactor

--- Containerfile
+++ Containerfile
@@ -82,6 +82,21 @@ RUN <<-'EOT' sh
 		zstd \
 		vlc \
 		vlc-plugins-all
+		
+	# Install Chrome Unstable for web development testing
+	mv /opt{,.bak} \
+    && mkdir /opt \
+    && dnf install -y --enablerepo="google-chrome" google-chrome-unstable \
+    && mv /opt/google/chrome /usr/lib/google-chrome \
+    && ln -sf /usr/lib/google-chrome/google-chrome /usr/bin/google-chrome-unstable \
+    && mkdir -p /usr/share/icons/hicolor/{16x16/apps,24x24/apps,32x32/apps,48x48/apps,64x64/apps,128x128/apps,256x256/apps} \
+    && for i in "16" "24" "32" "48" "64" "128" "256"; do \
+        ln -sf /usr/lib/google-chrome/product_logo_$i.png /usr/share/icons/hicolor/${i}x${i}/apps/google-chrome.png; \
+    done \
+    && rm -rf /etc/cron.daily \
+    && rmdir /opt/{google,} \
+    && mv /opt{.bak,} \
+    && dnf clean all
 	
 	# Remove specified GNOME shell extensions and apps
 	(rpm-ostree override remove \
--- scripts/012-install-brew.sh
+++ scripts/012-install-brew.sh
@@ -8,28 +8,31 @@ dnf install -y procps-ng curl file git gcc
 # Convince the installer we are in CI
 touch /.dockerenv
 
-# Make these so script will work
-mkdir -p /var/home
-mkdir -p /var/roothome
+# Make these directories so the script will work
+mkdir -p /var/home /var/roothome
 
-# Brew Install Script
-curl -Lo /tmp/brew-install https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
-chmod +x /tmp/brew-install
-/tmp/brew-install
-tar --zstd -cvf /usr/share/homebrew.tar.zst /home/linuxbrew/.linuxbrew
+# Check if Homebrew is already installed
+if [[ ! -d "/home/linuxbrew/.linuxbrew" ]]; then
+    curl -Lo /tmp/brew-install https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+    chmod +x /tmp/brew-install
+    /tmp/brew-install
+    tar --zstd -cvf /usr/share/homebrew.tar.zst /home/linuxbrew/.linuxbrew
+else
+    echo "Homebrew already installed, skipping installation."
+fi
 
 # Enable Systemd services
 systemctl enable brew-setup.service
 systemctl enable brew-upgrade.timer
 systemctl enable brew-update.timer
 
-# Clean up
-rm -rf /.dockerenv /var/home /var/roothome
+# Clean up installer but keep essential directories
+rm -rf /.dockerenv /tmp/brew-install
 
-# Register path symlink
-# We do this via tmpfiles.d so that it is created by the live system.
+# Register path symlink using tmpfiles.d
 cat >/usr/lib/tmpfiles.d/homebrew.conf <<EOF
 d /var/lib/homebrew 0755 1000 1000 - -
 d /var/cache/homebrew 0755 1000 1000 - -
 d /var/home/linuxbrew 0755 1000 1000 - -
 EOF
+
